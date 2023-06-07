a7b9ae9e78ab21f438bf10259eafba279330fb65
riobastian-bangkit <126978467+riobastian-bangkit@users.noreply.github.com>
2023-06-08 02:16:16 +0700

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ port = int(os.environ.get("PORT", 8080))
 class Item(BaseModel):
     query:str
 
-model = BertForSequenceClassification.from_pretrained("/app/model", from_tf=True)
+model = BertForSequenceClassification.from_pretrained("/app/model")
 tokenizer = BertTokenizer.from_pretrained("/app/tokenizer", local_files_only=True)
 data_rekomendasi = pd.read_csv("/app/data_rekomendasi.csv", sep=';')
 pipe = TextClassificationPipeline(model=model, tokenizer=tokenizer, top_k = 42)
